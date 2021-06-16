f52447f4739d06bded41d4478fb89e8de52d5c7c
ahujas7 <ahuja.saksham@outlook.com>
2021-06-15 17:35:54 -0700

    Food class with basic macros

---
+++ food.py
@@ -0,0 +1,13 @@
+import math
+
+class food():
+    def __init__(self, portion=0, cal=0, c=0, p=0, f=0):
+        self.portionSize = portion
+        self.calories = cal
+        self.carbs = c
+        self.protein = p
+        self.fats = f
+    
+    def __str__(self):
+         return f'Portion Size: {self.portionSize} g\nCalories: {self.calories} cal\nCarbs: {self.carbs} g\nProtein: {self.protein} g\nFats: {self.fats} g'
+
