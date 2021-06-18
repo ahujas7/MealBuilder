e966a9c49051a81d983d636bc92fe184e0ec349a
ahujas7 <ahuja.saksham@outlook.com>
2021-06-18 14:42:51 -0700

    selenium progress

--- selenium-test.py
+++ selenium-test.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+import pandas as pd
+
+
+
 
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 
@@ -8,13 +16,21 @@ driver = webdriver.Chrome(PATH)
 
 driver.get('https://www.realcanadiansuperstore.ca/')
 
+try:
+    search_bar = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "search-input__input"))
+    )
+
+    search_bar.send_keys('Suraj')
+    search_bar.send_keys(Keys.RETURN)
+except:
+    driver.quit()
+
 time.sleep(30)
 
-search = driver.find_element_by_class_name('search-input__input')
-search.send_keys('Suraj')
-search.send_keys(Keys.ENTER)
+results = driver.find_elements_by_class_name('product-tile__details__info__name__link')
 
-time.sleep(20)
+print('Number of results', len(results))
 
 driver.quit()
 
