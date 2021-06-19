bdc77fbf2c8ec50f7a06cc39f9954a2ec356ef85
ahujas7 <ahuja.saksham@outlook.com>
2021-06-19 14:49:28 -0700

    Adding notes

--- single_product.py
+++ single_product.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
@@ -10,22 +8,36 @@ import time
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 URL = 'https://www.realcanadiansuperstore.ca/cumin-seeds-whole/p/20618061_EA'
 
-
 driver = webdriver.Chrome(PATH)
-
 driver.get(URL)
 
+time.sleep(5)
 
-try:
-    nutrition_facts = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "nutrient-per-serving__label--calories"))
-    )
+driver.execute_script("window.scrollTo(0, 800)") 
 
-    print(nutrition_facts)
+nutrition_button = driver.find_element_by_class_name('product-details-page-nutrition-info__title')
+ActionChains(driver).move_to_element(nutrition_button).click(nutrition_button).perform()
 
-    # nutrition_file = open('nutrition.txt', 'w')
-    # nutrition_file.write(nutrition_facts.text)
-    # nutrition_file.close
+time.sleep(3)
 
-finally:
-    driver.quit()
+nutrition_facts = driver.find_element_by_class_name('product-nutrition')
+
+driver.quit()
+
+
+def retrieve_nutrition_data(driver, product_name, product_link):
+    driver.get(URL)
+
+    time.sleep(5)
+
+    driver.execute_script("window.scrollTo(0, 800)")
+
+    nutrition_button = driver.find_element_by_class_name('product-details-page-nutrition-info__title')
+    ActionChains(driver).move_to_element(nutrition_button).click(nutrition_button).perform()
+
+    time.sleep(3)
+
+    nutrition_facts = driver.find_element_by_class_name('product-nutrition')
+
+
+
